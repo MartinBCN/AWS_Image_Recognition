cfea2d8c0e24cf699ca419885227d27a5c64fdce
martin <martin-cleven@gmx.net>
2020-10-26 16:40:28 +0100

    Data from S3

--- localstack.sh
+++ localstack.sh
@@ -10,7 +10,7 @@ aws --endpoint-url=http://localhost:4566 s3 mb s3://data
 aws --endpoint-url=http://localhost:4566 s3 mb s3://models
 
 # Sync local directories with localstack S3
-aws --endpoint-url=http://localhost:4566 s3 sync data/mnist s3://data/mnistq
+aws --endpoint-url=http://localhost:4566 s3 sync data/mnist s3://data/mnist
 
 # Create Dynamo Tables
 #aws --endpoint-url=http://localhost:4569 dynamodb create-table --table-name SolarForecastResultsTable  --attribute-definitions AttributeName=regionCode,AttributeType=S AttributeName=time_period,AttributeType=N --key-schema AttributeName=regionCode,KeyType=HASH AttributeName=time_period,KeyType=RANGE --provisioned-throughput ReadCapacityUnits=500,WriteCapacityUnits=500
--- src/utils/mnist_reader.py
+++ src/utils/mnist_reader.py
@@ -1,8 +1,15 @@
-def load_mnist(path, kind='train'):
-    import os
-    import gzip
-    import numpy as np
+from io import BytesIO
+from unittest import mock
 
+import boto3
+import os
+import gzip
+import numpy as np
+import logging
+logger = logging.getLogger()
+
+
+def load_mnist(path, kind='train'):
     """Load MNIST data from `path`"""
     labels_path = os.path.join(path,
                                '%s-labels-idx1-ubyte.gz'
@@ -20,3 +27,62 @@ def load_mnist(path, kind='train'):
                                offset=16).reshape(len(labels), 784)
 
     return images, labels
+
+
+def mnist_s3(kind: str = 'train') -> (np.array, np.array):
+    """
+    Download MNIST from S3 Bucket
+
+    Parameters
+    ----------
+    kind
+
+    Returns
+    -------
+
+    """
+
+    logger.info(f'Load mnist data from S3 bucket')
+
+    # Note: endpoint url is only necessary in local development mode
+    #   if we pass None AWS constructs the correct URL. For the local test pipeline it needs to be in the
+    #   env variables
+    endpoint = os.environ.get('ENDPOINT_S3', None)
+    s3 = boto3.resource('s3', endpoint_url=endpoint)
+    bucket = s3.Bucket(os.environ['DATA_BUCKET'])
+    logger.info(f'Load data from {bucket}, endpoint {endpoint}')
+
+    # Prefix depends on region and profile type
+    prefix = f''
+    logger.info(f'Getting file list in {prefix}')
+
+    fn = f'mnist/{kind}-labels-idx1-ubyte.gz'
+    obj = s3.Object(os.environ['DATA_BUCKET'], fn)
+    n = obj.get()['Body'].read()
+    gzipfile = gzip.GzipFile(fileobj=BytesIO(n))
+    content = gzipfile.read()
+    labels = np.frombuffer(content, dtype=np.uint8, offset=8)
+
+    fn = f'mnist/{kind}-images-idx3-ubyte.gz'
+    obj = s3.Object(os.environ['DATA_BUCKET'], fn)
+    n = obj.get()['Body'].read()
+    gzipfile = gzip.GzipFile(fileobj=BytesIO(n))
+    content = gzipfile.read()
+    images = np.frombuffer(content, dtype=np.uint8, offset=8)
+
+    return images, labels
+
+
+if __name__ == '__main__':
+    logger.setLevel(logging.DEBUG)
+    ch = logging.StreamHandler()
+    ch.setLevel(logging.INFO)
+    formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+    ch.setFormatter(formatter)
+    logger.addHandler(ch)
+
+    os.environ['ENDPOINT_S3'] = 'http://localhost:4566'
+    os.environ['DATA_BUCKET'] = 'data'
+
+    x, y = mnist_s3()
+
